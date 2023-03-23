edf39810c3c38523fb258c836461bd84a80df8ec
Historia Montague <historia.montague@outlook.com>
2023-03-23 04:19:30 -0600

    Gosh darned finished that there lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const thanks = [];
+    for(let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return(thanks);
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
